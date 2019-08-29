7710a1e30b0d8102d637249527e1827092b87b96
shirleytwl <shirtanwl@gmail.com>
2019-08-29 13:44:09 +0800

    able to add to list, display list items and validate

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ![https://mir-s3-cdn-cf.behance.net/project_modules/max_3840/14de0e61805871.5a7a7f88d1696.jpg](https://mir-s3-cdn-cf.behance.net/project_modules/max_3840/14de0e61805871.5a7a7f88d1696.jpg)
 
-Start with the starter code in script.js.
+Start with the starter code in script.jsx.
 
 Do `npm install` to get and run `http-server`
 (you will need it so that script.jsx works correctly)
---
+++ script.jsx
@@ -0,0 +1,93 @@
+
+class List extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			word: "",
+			list: [],
+			validated: false,
+			typed: false
+		}
+	}
+
+	addItem(e) {
+		if (this.state.validated) {
+			let list = this.state.list;
+			list.push(this.state.word);
+			this.setState({list: list});
+			this.setState({word: ""});
+			this.setState({typed: false})
+		}
+	}
+
+	removeItem(e, index) {
+		let list = this.state.list;
+		list.splice(index, 1);
+		this.setState({list: list});
+	}
+
+	changeHandler(e) {
+		let value = e.target.value;
+		this.setState({word: value});
+		if (value.length <= 0 || value.length > 20) {
+			this.setState({validated: false})
+		} else {
+			this.setState({validated: true})
+		}
+		this.setState({typed: true})
+	}
+
+	render() {
+		// render the list with a map() here
+
+		let listItems = this.state.list.map((item, index) => {
+			return (
+				<li className="list-group-item" key={index}>
+					<p>{index + 1}. {item}</p>
+					<div className="btn btn-danger" onClick={(e) => {
+						this.removeItem(e, index)
+					}}>Delete
+					</div>
+				</li>);
+		});
+		let errorMessage = null;
+		if (!this.state.validated && this.state.typed) {
+			errorMessage = <div className="alert alert-danger" role="alert">
+				must be more than 1 character and less than 20 characters
+			</div>
+		} else {
+			errorMessage = null;
+		}
+		return (
+			<div className="wrapper">
+				<h2 className="mb-5">To-Do List</h2>
+				<input className="form-control" onChange={(e) => {
+					this.changeHandler(e)
+				}} value={this.state.word}/>
+				<button className="my-3 btn btn-primary" onClick={(e) => {
+					this.addItem(e)
+				}}>add item
+				</button>
+				{errorMessage}
+				<div className="list mt-5">
+					<ul className="list-group">
+						{listItems}
+					</ul>
+				</div>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<div className="container my-5">
+		<div className="row">
+			<div className="col">
+				<List/>
+			</div>
+		</div>
+	</div>
+	, document.getElementById('root')
+);
+
